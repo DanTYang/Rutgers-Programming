f6e1362f2319acbe42e07f7c1217fe4e2611a5b5
Martin Kong <32230581+MartinKong68@users.noreply.github.com>
2020-02-29 14:01:41 -0500

    Update rpthread.c

--- OS/Project2/rpthread.c
+++ OS/Project2/rpthread.c
@@ -139,24 +139,7 @@ static void schedule() {
 	// else if (sched == MLFQ)
 	// 		sched_mlfq();
 
-	// YOUR CODE HER
-rpthread.a: rpthread.o
-	$(AR) librpthread.a rpthread.o
-	$(RANLIB) librpthread.a
-
-rpthread.o: rpthread.h
-
-ifeq ($(SCHED), PSJF)
-	$(CC) -pthread $(CFLAGS) rpthread.c
-else ifeq ($(SCHED), MLFQ)
-	$(CC) -pthread $(CFLAGS) -DMLFQ rpthread.c
-else
-	echo "no such scheduling algorithm"
-endif
-
-clean:
-	rm -rf testfile *.o *.a
-E
+	// YOUR CODE HERE
 
 // schedule policy
 #ifndef MLFQ
